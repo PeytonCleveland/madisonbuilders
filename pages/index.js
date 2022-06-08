5a7a375810ad4cacf0ea067db3c56b149a92da02
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-06-08 16:52:56 -0500

    content

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
     <div className="w-full flex">
-      <h1 className="text-indigo-900 font-bold text-4xl">
-        Alabama's premier custom home builder
-      </h1>
+      <h1 className="text-indigo-900 font-bold text-4xl">Home Page</h1>
     </div>
   );
 };
