3b5bb461be6c43a0e5d9a9c4dc3829def091c80e
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-06-09 15:31:49 -0500

    height change

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 const Home = () => {
   return (
     <>
-      <div className="container h-[45vh] flex flex-col justify-end">
+      <div className="container h-[55vh] flex flex-col justify-end">
         <h1 className="text-indigo-900 text-3xl mb-2">
           Alabama&apos;s Premier <strong>Custom Home Builder</strong>
         </h1>
         <h2 className="text-gray-700 font-sans font-light">
           Lorem ipsum dolor sit amet consectetor adpiscing elit sed do eiusmod
         </h2>
-        <div className="flex mt-6 mb-24">
+        <div className="flex mt-6 mb-28">
           <button className="flex flex-1 mr-2 justify-center items-center border-2 border-indigo-900 text-indigo-900 px-5 py-[5px] font-semibold text-lg rounded hover:bg-indigo-800 hover:scale-105 duration-150">
             Our process
           </button>
@@ -24,7 +24,7 @@ const Home = () => {
           playsinline
           src="/stock.mov"
         ></video>
-        <h3 className="text-white text-xl mb-2">
+        <h3 className="text-white text-xl mb-10">
           Discover <strong>Our Difference</strong>
         </h3>
       </div>
