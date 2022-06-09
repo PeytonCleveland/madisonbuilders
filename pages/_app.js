bb40122e4eabec26da371ea6259b725efd0dba91
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-06-09 12:22:25 -0500

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,31 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Madison Builders</title>
+        <meta type="og:title" content="Madison Builders" />
+        <meta
+          type="og:description"
+          content="Madison Builders is Alabama's premier custom home builder, focused on utilizing the latest in building science to construct high-performing, energy-efficient homes."
+        />
+        <meta type="og:author" content="Peyton Cleveland" />
+        <meta type="og:url" content="https://madisonbuilders.com" />
+        <meta
+          type="og:image"
+          content="https://madisonbuilders.com/madison-builders.png"
+        />
+        <meta type="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@madisonbuilders" />
+        <meta name="twitter:creator" content="@madisonbuilders" />
+        <meta name="twitter:title" content="Madison Builders" />
+        <meta
+          name="twitter:description"
+          content="Madison Builders is Alabama's premier custom home builder, focused on utilizing the latest in building science to construct high-performing, energy-efficient homes."
+        />
+        <meta
+          name="twitter:image"
+          content="https://madisonbuilders.com/madison-builders.png"
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
