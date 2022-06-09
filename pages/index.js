7f1018fdd2dc7e0588dc7a539e1c78737c7e08d7
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-06-09 11:27:33 -0500

    fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="w-full h-[70vh] flex flex-col justify-center">
       <h1 className="text-indigo-900 text-3xl mb-2">
-        Alabama's Premier <strong>Custom Home Builder</strong>
+        Alabama&apos;s Premier <strong>Custom Home Builder</strong>
       </h1>
       <h2 className="text-gray-600 font-sans font-light">
         Lorem ipsum dolor sit amet consectetor adpiscing elit sed do eiusmod
