e5d0405bf299684444d26603fc96d3470bb722ca
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-06-10 18:10:37 -0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 const Home = () => {
   return (
     <>
-      <div className="container h-[500px] flex flex-col justify-end">
+      <div className="container h-[500px] flex flex-col justify-end bg-white">
         <h1 className="text-indigo-900 text-3xl mb-2">
           Alabama&apos;s Premier <strong>Custom Home Builder</strong>
         </h1>
@@ -40,7 +42,7 @@ const Home = () => {
         </button>
       </div>
 
-      <div className="w-screen container bg-indigo-100 flex flex-col py-10">
+      <div className="w-screen container bg-gradient-to-br from-indigo-100 to-indigo-200 flex flex-col py-10">
         <h3 className="text-indigo-900 text-xl mb-2">
           Our <strong>Process</strong>
         </h3>
@@ -101,7 +103,57 @@ const Home = () => {
         </p>
       </div>
 
-      <div className="w-screen h-[400px] container bg-gray-900 flex flex-col justify-center"></div>
+      <div className="w-screen container bg-gradient-to-br from-gray-800 to-gray-900 flex flex-col pt-8">
+        <h3 className="text-white text-xl mb-2">
+          Unmatched <strong>Transparency</strong>
+        </h3>
+        <p className="text-gray-200 font-light font-sans text-sm mb-4 pr-8">
+          Madison Builders' customer portal allows unparalleled transparency
+          into the homebuilding process.
+        </p>
+        <Link href="/" passHref>
+          <a className="mb-4 text-teal-300 flex items-center text-sm font-sans">
+            Explore customer portal
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+        </Link>
+        <h3 className="text-white mb-3">
+          Choose finishes <strong>for your home</strong>
+        </h3>
+        <div className="w-full h-52 bg-gray-600 mb-8"></div>
+        <h3 className="text-white mb-3">
+          Make change orders <strong>in minutes</strong>
+        </h3>
+        <div className="w-full h-52 bg-gray-600 mb-8"></div>
+        <h3 className="text-white mb-3">
+          Easily track <strong>updates and expenses</strong>
+        </h3>
+        <div className="w-full h-52 bg-gray-600 -mb-10"></div>
+      </div>
+
+      <div className="container h-64 pt-20">
+        <h3 className="text-indigo-900 text-xl mb-2">
+          Mortgage <strong>Calculator</strong>
+        </h3>
+        <p className="text-gray-800 font-light font-sans text-sm mb-4 pr-8">
+          Find out how much home you can afford.
+        </p>
+        <input
+          type="text"
+          className="w-full h-10 px-4 py-2 bg-gray-200 rounded-lg shadow-md focus:outline-none focus:bg-gray-300"
+        />
+      </div>
     </>
   );
 };
