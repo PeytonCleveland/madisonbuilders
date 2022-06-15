53ae24047580772af1a6b8cfae5fbe9a211179d8
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-06-15 13:01:12 -0500

    portal

--- pages/client-portal/index.js
+++ pages/client-portal/index.js
@@ -5,9 +5,50 @@ const ClientPortal = () => {
   const { data: session } = useSession();
   return (
     <div className="container pt-24 pb-32 flex flex-col gap-6 bg-gradient-to-br from-gray-50 to-gray-100">
-      <h1 className="text-white font-semibold text-lg bg-gradient-to-br from-indigo-700 to-indigo-800 p-3 rounded-lg shadow-md">{`${
-        getGreeting().split("/")[1]
-      }, ${session?.userData.firstName}!`}</h1>
+      <div className="flex flex-col gap-5 bg-gradient-to-br from-indigo-700 to-indigo-800 p-4 pb-5 rounded-lg shadow-md">
+        <h1 className="text-white font-semibold text-lg">{`${getGreeting()}, ${
+          session?.userData.firstName
+        }!`}</h1>
+        <div className="flex flex-col gap-1.5">
+          <div className="flex justify-between items-center">
+            <p className="text-white font-light text-sm">Project progress</p>
+            <p className="text-white font-light text-xs font-sans">
+              2% Complete
+            </p>
+          </div>
+          <div className="w-full bg-indigo-500 rounded-full h-2">
+            <div
+              className="bg-teal-300 h-2 rounded-full"
+              style={{ width: "4%" }}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <p className="text-white font-light text-sm">Next step:</p>
+          <button className="bg-white text-indigo-800 rounded-full shadow-md py-2 flex items-center justify-center gap-1 font-semibold">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Contract signature
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <div className="flex justify-between items-center">
+          <p className="text-gray-900 font-medium">Quick actions</p>
+        </div>
+      </div>
+
       <div className="flex flex-col gap-3">
         <div className="flex justify-between items-center">
           <p className="text-gray-900 font-medium">My project details</p>
@@ -28,7 +69,7 @@ const ClientPortal = () => {
             Edit info
           </button>
         </div>
-        <div className="flex flex-col gap-3 bg-white p-5 rounded-lg shadow-md">
+        <div className="flex flex-col gap-3 bg-white p-4 rounded-lg shadow-md">
           <div className="flex flex-col gap-0.5">
             <p className="text-xs text-gray-600 font-sans">Project name</p>
             <h6>305 Brownstone Loop</h6>
@@ -45,6 +86,12 @@ const ClientPortal = () => {
             <p className="text-xs text-gray-600 font-sans">Financing</p>
             <h6>Cash - No Financing</h6>
           </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <div className="flex justify-between items-center">
+          <p className="text-gray-900 font-medium">Support</p>
         </div>
       </div>
     </div>
--- utils/get-greeting.js
+++ utils/get-greeting.js
@@ -2,7 +2,7 @@ const getGreeting = () => {
   const now = new Date();
   const hour = now.getHours();
   if (hour < 12) {
-    return "☕️/Good morning";
+    return "Good morning";
   } else if (hour < 18) {
     return "Good afternoon";
   } else {
