584a2709a93b79e977d73594a8baedfd1fe6be9b
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-07-13 16:16:22 -0500

    api fixed fr

--- pages/api/plans.js
+++ pages/api/plans.js
@@ -2,9 +2,9 @@ import { db } from "../../firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
 export default async function handler(req, res) {
-  const resQuery = req.query;
-  const { planNumber } = resQuery;
-  console.log(planNumber);
+  const {
+    query: { planNumber }
+  } = req;
 
   const q = query(
     collection(db, "plans"),
@@ -12,9 +12,7 @@ export default async function handler(req, res) {
   );
 
   const querySnapshot = await getDocs(q);
-  const plan = querySnapshot.docs.map((p) => {
-    p.data();
-  });
+  const plan = querySnapshot.docs[0].data();
 
   plan.length === 0 ? res.status(404) : res.status(200).json(plan);
 }
--- pages/plans/[plan].js
+++ pages/plans/[plan].js
@@ -1,6 +1,8 @@
 const Plan = ({ plan }) => {
   return (
-    <div className="container flex flex-col w-full pt-36 pb-12">{plan}</div>
+    <div className="container flex flex-col w-full pt-36 pb-12">
+      {plan.planNumber}
+    </div>
   );
 };
 
@@ -9,7 +11,7 @@ export default Plan;
 export async function getServerSideProps(context) {
   const query = context.query;
   const plan = await fetch(
-    `https://madisonbuilders.llc/api/plans?planNumber=${query.plan}`
+    `http://localhost:3000/api/plans?planNumber=${query.plan}`
   );
   return {
     props: { plan: await plan.json() }
