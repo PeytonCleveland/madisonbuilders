6fda88453c32d3b0e26cc843935d35ff0b66f77c
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-07-21 13:31:30 -0500

    hide blog

--- components/header/index.js
+++ components/header/index.js
@@ -12,8 +12,7 @@ const links = [
   { text: "About", href: "/about" },
   { text: "Process", href: "/process" },
   { text: "Work", href: "/work" },
-  { text: "Plans", href: "/plans" },
-  { text: "Blog", href: "/blog" }
+  { text: "Plans", href: "/plans" }
 ];
 
 const Header = () => {
